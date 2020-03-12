4c13403f2d7cabc31aad0b0b8cf53b801a213eb4
rzfire <53797893+rzfire@users.noreply.github.com>
2020-03-11 20:19:21 -0400

    Add files via upload

---
+++ problem3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#1. Use python3 to run the script TC_1.py and redirect stdout (but not stderr!) to the filename 'test1.txt' and stderr (but not stdout!) to the filename 'error1.txt'
+
+
+#2. Use python3 to run the script TC_2.py and redirect stdout (but not stderr!) to the filename 'test2.txt' and stderr (but not stdout!) to the filename 'error2.txt'
+
+
+#3. Produce the difference between TC1 and test1.txt and redirect the output to `student.txt`.
+
+
+#4. Produce the difference between TC2 and test2.txt and **append** the output to `student.txt`. Please remember there is a difference between `>` and `>>`. In this step you are *appending* to a pre-existing file that already has data you do not want to over-write.
+
+
